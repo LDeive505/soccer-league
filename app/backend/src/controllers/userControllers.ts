07d3e394d2de2d810c581101451909ddc6a1f6f5
LDeive505 <luisdaviab@gmail.com>
2022-10-01 20:42:43 -0300

    feat: tests the validations implemented for the login route

--- app/backend/src/controllers/userControllers.ts
+++ app/backend/src/controllers/userControllers.ts
@@ -6,7 +6,6 @@ export default class userControllers {
 
   public login: RequestHandler = async (req, res) => {
     const token = await this.userServices.login(req.body);
-    if (!token) return res.status(401).json({ message: 'Incorrect email or password' });
     return res.status(200).json({ token });
   };
 }
--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -7,25 +7,50 @@ import userModel from '../database/models/User';
 import { User } from '../types/UserTypes';
 import { Response } from 'superagent';
 
+
 const { expect } = chai;
 
-/* const userMock = { 
-  id: 1, username: 'Admin', role: 'Admin', email: 'admin@email.com',
-  password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW' 
-}; */
-
 const loginMock = { email: 'user@user.com', password: 'secret_user' };
+const invalidLoginMock = { email: 'zyzz@email', password: '123456' };
 
 chai.use(chaiHttp);
 
 describe('1 - Test login route', async () => {
-  it('Should return status code 200', async () => {
-    const response = await chai.request(app).post('/login').send(loginMock);
-    expect(response.status).to.be.equal(200);
+  describe('1.1 - Test login route with valid credentials', async () => {
+    it('Should return status code 200', async () => {
+      const response = await chai.request(app).post('/login').send(loginMock);
+      expect(response.status).to.be.equal(200);
+    });
+    it('with a valid token', async () => {
+      const response = await chai.request(app).post('/login').send(loginMock);
+      expect(response.body).to.haveOwnProperty('token');
+      expect(response.body.token).to.be.a('string');
+    });
   });
-  it('with a valid token', async () => {
-    const response = await chai.request(app).post('/login').send(loginMock);
-    expect(response.body).to.haveOwnProperty('token');
-    expect(response.body.token).to.be.a('string');
+
+  describe('1.2 - Test login route without credentials', async () => {
+    it('Should return status code 400', async () => {
+      const response = await chai.request(app).post('/login');
+      expect(response.status).to.be.equal(400);
+    });
+    it('with a message', async () => {
+      const response = await chai.request(app).post('/login');
+      expect(response.body).to.haveOwnProperty('message');
+      expect(response.body.message).to.be.a('string');
+      expect(response.body.message).to.be.equal('All fields must be filled');
+    });
+  });
+
+  describe('1.2 - Test login route with invalid credentials', async () => {
+    it('Should return status code 401', async () => {
+      const response = await chai.request(app).post('/login').send(invalidLoginMock);
+      expect(response.status).to.be.equal(401);
+    });
+    it('with a message', async () => {
+      const response = await chai.request(app).post('/login').send(invalidLoginMock);
+      expect(response.body).to.haveOwnProperty('message');
+      expect(response.body.message).to.be.a('string');
+      expect(response.body.message).to.be.equal('Incorrect email or password');
+    });
   });
 });
