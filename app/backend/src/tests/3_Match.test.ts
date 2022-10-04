bcb2c3194617607d6bdca7ae0ba1d00dbf2b8a3b
LDeive505 <luisdaviab@gmail.com>
2022-10-04 18:40:14 -0300

    test: refactor tests for all /matches routes

--- app/backend/src/tests/3_Match.test.ts
+++ app/backend/src/tests/3_Match.test.ts
@@ -4,46 +4,66 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http'); 
 import { app } from '../app';
 import matchesMock from './mocks/matches';
+import { Response } from 'superagent';
 // import matchModel from '../database/models/Match';
 
 const { expect } = chai;
 
-describe('3 - Test /match routes', async () => {
+describe('3 - Test /matches routes', async () => {
 
   const inProgressMock = matchesMock.filter((match) => match.inProgress);
   const notInProgressMock = matchesMock.filter((match) => !match.inProgress);
 
-  describe('2.1 - Test /match route with GET metod', async () => {
+  describe('3.1 - Test /matches route with GET metod', async () => {
+    let response: Response;
+
+    before(async () => {
+      response = await chai.request(app).get('/matches');
+    });
+
     it('Should return status code 200', async () => {
-      const response = await chai.request(app).get('/matches');
       expect(response.status).to.be.equal(200);
     });
+
     it('with all matches data in the database including the ones in the Teams table', async () => {
-      const response = await chai.request(app).get('/matches');
       expect(response.body).to.be.a('array');
       expect(response.body).to.be.deep.equal(matchesMock);
     });
   });
 
-  describe('2.2 - Test /match route with GET metod using query string', async () => {
+  describe('3.2 - Test /matches route with GET method using query string with inProgress=true', async () => {
+    let response: Response;
+
+    before(async () => {
+      response = await chai.request(app).get('/matches?inProgress=true');
+    });
+
     it('Should return status code 200', async () => {
-      const response = await chai.request(app).get('/matches?inProgress=true');
       expect(response.status).to.be.equal(200);
     });
+
     it('with all matches in progress in the database', async () => {
-      const response = await chai.request(app).get('/matches?inProgress=true');
       expect(response.body).to.be.a('array');
       expect(response.body).to.be.deep.equal(inProgressMock);
     });
+  });
+
+  describe('3.3 - Test /matches route with GET method using query string with inProgress=false', async () => {
+    let response: Response;
+
+    before(async () => {
+      response = await chai.request(app).get('/matches?inProgress=false');
+    });
+
     it('Should return status code 200', async () => {
-      const response = await chai.request(app).get('/matches?inProgress=false');
       expect(response.status).to.be.equal(200);
     });
+
     it('with all finished matches', async () => {
-      const response = await chai.request(app).get('/matches?inProgress=false');
       expect(response.body).to.be.a('array');
       expect(response.body).to.be.deep.equal(notInProgressMock);
     });
   });
+
 });
 
