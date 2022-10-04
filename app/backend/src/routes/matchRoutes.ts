bde46ded912b1d4f22594718eac0c7e5df2112a3
LDeive505 <luisdaviab@gmail.com>
2022-10-04 14:13:30 -0300

    feat: add token validation to /matches route in GET method

--- app/backend/src/routes/matchRoutes.ts
+++ app/backend/src/routes/matchRoutes.ts
@@ -3,6 +3,7 @@ import MatchServices from '../services/matchServices';
 import MatchControllers from '../controllers/matchControllers';
 import Match from '../database/models/Match';
 import matchValidator from '../middlewares/matchValidator';
+import tokenValidator from '../middlewares/tokenValidation';
 
 const router = Router();
 
@@ -10,7 +11,7 @@ const matchControllers = new MatchControllers(new MatchServices(Match));
 
 router.route('/')
   .get(matchControllers.getMatches)
-  .post(matchValidator, matchControllers.createMatch);
+  .post(tokenValidator, matchValidator, matchControllers.createMatch);
 
 router.route('/:id/finish').patch(matchControllers.updateMatch);
 
