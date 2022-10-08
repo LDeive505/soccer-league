ad1f02f8c57b50c2bfadd21601766429b7a706c2
LDeive505 <luisdaviab@gmail.com>
2022-10-08 18:19:23 -0300

    test: add testes for /leaderboard e leaderboard/away routes for the get method

--- app/backend/src/tests/4_Leaderboard.test.ts
+++ app/backend/src/tests/4_Leaderboard.test.ts
@@ -3,6 +3,8 @@ import * as chai from 'chai';
 // @ts-ignore
 import chaiHttp = require('chai-http'); 
 import { app } from '../app';
+import { leaderboardHomeMock } from './mocks/leaderboardHome';
+import { leaderboardAwayMock } from './mocks/leaderboardAway';
 import { leaderboardMock } from './mocks/leaderboard';
 import { Response } from 'superagent';
 
@@ -12,13 +14,43 @@ const { expect } = chai;
 chai.use(chaiHttp);
 
 describe('4 - Test /leaderboard routes', async () => {
-  let response: Response;
+  describe('4.1 - Test /leaderboard/home route', async () => {
+    let response: Response;
 
-  before(async () => {
-    response = await chai.request(app).get('/leaderboard/home');
+    before(async () => {
+      response = await chai.request(app).get('/leaderboard/home');
+    });
+
+    it('Should return status code 200', async () => {
+      expect(response.status).to.be.equal(200);
+    });
+    it('with the expected data', async () => {
+      expect(response.body).to.be.deep.equal(leaderboardHomeMock);
+    });
   });
 
-  describe('4.1 - Test /leaderboard/home route', async () => {
+  describe('4.2 - Test /leaderboard/away route', async () => {
+    let response: Response;
+
+    before(async () => {
+      response = await chai.request(app).get('/leaderboard/away');
+    });
+
+    it('Should return status code 200', async () => {
+      expect(response.status).to.be.equal(200);
+    });
+    it('with the expected data', async () => {
+      expect(response.body).to.be.deep.equal(leaderboardAwayMock);
+    });
+  });
+
+  describe('4.3 - Test /leaderboard route', async () => {
+    let response: Response;
+
+    before(async () => {
+      response = await chai.request(app).get('/leaderboard');
+    });
+
     it('Should return status code 200', async () => {
       expect(response.status).to.be.equal(200);
     });
